ecffbf4af24b6f83ee588c96203eea687c5f2abe
Borislav Tserovski <Borislav.Tserovski@gmail.com>
2017-07-20 15:46:31 +0300

    Scale

---
+++ Scale/Scale.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Scale<T> where T : IComparable<T>
+{
+    private T left;
+    private T right;
+
+    public Scale(T left, T right)
+
+    {
+        this.Left = left;
+        this.Right = right;
+    }
+
+    public T Left
+    {
+        get { return this.left; }
+        private set { this.left = value; }
+    }
+    public T Right
+    {
+        get { return this.right; }
+        private set { this.right = value; }
+    }
+
+
+
+    public int CompareTo(T other)
+    {
+        throw new NotImplementedException();
+    }
+    public T GetHavier()
+    {
+        if (left.CompareTo(right) > 0)
+        {
+            return left;
+        }
+        else if (left.CompareTo(right) < 0)
+        {
+            return right;
+        }
+        return default(T);
+    }
+}
+
