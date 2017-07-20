7f441915087e1004f90c345f16109ed3d351ea40
Borislav Tserovski <Borislav.Tserovski@gmail.com>
2017-07-20 14:35:45 +0300

    Generics

---
+++ Box/Box.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class Box<T>
+{
+    private IList<T> items;
+
+    public Box()
+    {
+        this.items = new List<T>();
+    }
+
+    public int Count
+    {
+        get { return this.items.Count; }
+    }
+
+    public void Add(T element)
+    {
+        this.items.Add(element);
+    }
+
+    public T Remove()
+    {
+        var reminder = items.LastOrDefault();
+        this.items.RemoveAt(items.Count - 1);
+        return reminder;
+    }
+}
+
