91af3f22bca46599033187494b3682a3e339e358
Borislav Tserovski <Borislav.Tserovski@gmail.com>
2017-07-20 15:09:21 +0300

    ArrayCreator

---
+++ ArrayCreator/ArrayCreator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+public class ArrayCreator
+{
+    public static T[] Create<T>(int lenght, T item)
+    {
+       
+            return new T[lenght];
+        
+       
+        
+    }
+}
+
