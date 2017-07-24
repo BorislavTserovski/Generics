d0d023e4d299ba9bb24f2b239502489f46121b9d
Borislav Tserovski <Borislav.Tserovski@gmail.com>
2017-07-24 23:53:39 +0300

    Threeuple

---
+++ Tuple/StartUp.cs
@@ -0,0 +1,30 @@
+﻿namespace Tuples
+{
+    using System;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var inputTokens = Console.ReadLine().Split();
+            var names = $"{inputTokens[0]} {inputTokens[1]}";
+            var address = inputTokens[2];
+            var town = inputTokens[3];
+            var namesAdress = new Threeuple<string, string, string>(names, address, town);
+            Console.WriteLine(namesAdress);
+            inputTokens = Console.ReadLine().Split();
+            var name = inputTokens[0];
+            var liters = int.Parse(inputTokens[1]);
+            var isDrunk = inputTokens[2] == "drunk";
+            var nameLiters = new Threeuple<string, int, bool>(name, liters, isDrunk);
+            Console.WriteLine(nameLiters);
+            inputTokens = Console.ReadLine().Split();
+            var person = inputTokens[0];
+            var doubleItem = double.Parse(inputTokens[1]);
+            var bank = inputTokens[2];
+            var tuple = new Threeuple<string, double,string>(person, doubleItem, bank);
+            Console.WriteLine(tuple);
+            
+        }
+    }
+}
---
+++ Tuple/Threeuple.cs
@@ -0,0 +1,22 @@
+﻿namespace Tuples
+{
+    public class Threeuple<T, U, V>
+    {
+        private T item1;
+        private U item2;
+        private V item3;
+        public Threeuple(T item1, U item2, V item3)
+        {
+            this.item1 = item1;
+            this.item2 = item2;
+            this.item3 = item3;
+        }
+
+       
+
+        public override string ToString()
+        {
+            return $"{this.item1} -> {this.item2} -> {this.item3}";
+        }
+    }
+}
